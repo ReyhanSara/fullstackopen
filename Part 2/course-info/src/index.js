38d3c1aea39f5026bd9ba8b65984b3f8a43f614d
Sara <reyhansara97@gmail.com>
2020-10-28 18:46:26 +0530

    Remove unnecessary comments

--- Part 2/course-info/src/index.js
+++ Part 2/course-info/src/index.js
@@ -5,5 +5,4 @@ import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
   
-     //set REACT_APP_API_KEY='55a563616c1a1d3d67f9f9ae438543e6' && npm start
-    //https://api.openweathermap.org/data/2.5/weather?q=Finland&appid=55a563616c1a1d3d67f9f9ae438543e6'
+  
